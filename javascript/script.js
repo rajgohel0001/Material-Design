893e03027f078ced50d8945eadf093a8849081ae
rajgohel0001 <rajgohel855@gmail.com>
2019-02-23 18:23:29 +0530

    MD

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+// const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
